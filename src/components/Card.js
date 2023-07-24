8b3e5a852a787dbd28df413b83e2f41eb71a7f78
imisky <yehforengineer@gmail.com>
2023-07-24 16:32:34 +0800

    update: 2023-07-24 16:32:25

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,36 @@ import './Card.css';
 const Card = ({ data,index }) => {
   const { vocabulary, part_of_speech, zh_cn } = data;
 
-  return (
-    <div className="card">
+  // 翻轉卡片
+  const [isFlipped, setIsFlipped] = useState(false);
 
-      <h2>{vocabulary}</h2>
-      <p> {part_of_speech}.</p>
-      <p>-{zh_cn}</p>
-      <p className="index">#{index}</p>
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <div className={`card ${isFlipped ? "flipped" : ""}`} onClick={handleClick}>
+      <div className="front">
+        <h2>{vocabulary}</h2>
+        <p> {part_of_speech}.</p>
+        <p>-{zh_cn}</p>
+        <p className="index">#{index}</p>
+      </div>
+      <div className="back">
+        <h2>{vocabulary}</h2>
+        <p> {part_of_speech}.</p>
+        <p>-{zh_cn}</p>
+        <p className="index">#{index}</p>
+      </div>
     </div>
+
+    // <div className="card">
+
+    //   <h2>{vocabulary}</h2>
+    //   <p> {part_of_speech}.</p>
+    //   <p>-{zh_cn}</p>
+    //   <p className="index">#{index}</p>
+    // </div>
   );
 };
 
