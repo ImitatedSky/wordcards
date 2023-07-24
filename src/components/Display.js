ba57aab50c1401449161afe543c07f173f9713de
imisky <yehforengineer@gmail.com>
2023-07-24 15:42:04 +0800

    update: 2023-07-24 15:42:01

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,23 +1,37 @@
 import React, { useState, useEffect } from "react";
 import Card from "./Card";
-import { parseExcelFile } from "./excelUtils"; //  excelUtils.js
+import { parseExcelFile } from "./excelUtils"; // excelUtils.js
 
 const Display = () => {
   const [cardDataArray, setCardDataArray] = useState([]);
+  const [selectedSheetIndex, setSelectedSheetIndex] = useState(0);
 
   useEffect(() => {
-    // 讀取 voc.xlsx 並更新 cardDataArray
-    // parseExcelFile((data)
-    parseExcelFile((data) => {
+    // 自動讀取 voc.xlsx 文件並更新 cardDataArray
+    parseExcelFile(selectedSheetIndex, (data) => {
       setCardDataArray(data);
     });
-  }, []); // 僅執行一次
+  }, [selectedSheetIndex]); // 當 selectedSheetIndex 變化時重新執行
+
+  const handleSheetChange = (index) => {
+    setSelectedSheetIndex(index);
+  };
 
   return (
     <div className="card-container">
       {cardDataArray.map((data, index) => (
         <Card key={index} data={data} />
       ))}
+
+      {/* 新增按鈕來改變工作表 */}
+      <div className="sheet-buttons">
+        <button onClick={() => handleSheetChange(0)}>1級</button>
+        <button onClick={() => handleSheetChange(1)}>2級</button>
+        <button onClick={() => handleSheetChange(2)}>3級</button>
+        <button onClick={() => handleSheetChange(3)}>4級</button>
+        <button onClick={() => handleSheetChange(4)}>5級</button>
+        <button onClick={() => handleSheetChange(5)}>6級</button>
+      </div>
     </div>
   );
 };
